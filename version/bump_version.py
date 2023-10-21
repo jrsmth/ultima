9ad6de907abc3bb6cf2845c4df4fc4305ff3b123
JRSmiffy <jamesrichardsmith97@gmail.com>
2023-10-21 13:58:43 +0100

    UMA-4: gh action - added versioning

---
+++ version/bump_version.py
@@ -0,0 +1,13 @@
+from version import __version__
+
+# Bump Version to Next Incremental and Add Snapshot:
+# python version/bump_version.py
+handler = open("./version/version.py", "w")
+major = __version__.split(".")[0]
+minor = __version__.split(".")[1]
+incremental = int(__version__.split(".")[2]) + 1
+
+handler.write(f'__version__ = "{major}.{minor}.{incremental}-SNAPSHOT"')
+handler.close()
+
+print(f"{major}.{minor}.{incremental}-SNAPSHOT")
---
+++ version/release_snapshot.py
@@ -0,0 +1,10 @@
+from version import __version__
+
+# Release Snapshot:
+# python version/release_snapshot.py
+handler = open("./version/version.py", "w")
+released_version = __version__.split("-")[0]
+handler.write(f'__version__ = "{released_version}"')
+handler.close()
+
+print(released_version)
