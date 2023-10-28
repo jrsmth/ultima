e5a325c56376174062474865bf82c4d0b93ea6ee
jrsmth <jamesrichardsmith97@gmail.com>
2023-10-28 06:48:35 +0100

    [patch] UMA-10: created dev mode

--- config.py
+++ config.py
@@ -2,5 +2,9 @@ class Config(object):
     REDIS_URL = "redis://@localhost:6379/0"
 
 
+class DevConfig(object):
+    REDIS_URL = "rediss://red-cklv99jj89us738u33i0:c1vXNdmZhDpXxxOTrkEsaBM9m9Kgk67z@frankfurt-redis.render.com:6379"
+
+
 class ProductionConfig(Config):
     REDIS_URL = 'redis://red-cklv99jj89us738u33i0:6379'
